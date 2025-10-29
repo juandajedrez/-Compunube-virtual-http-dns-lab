2b468dc409fee4c5a4b3d78770825ceb876a7f9b
Jahn Martinez <jahn8916@gmail.com>
2025-10-29 15:06:27 -0500

    eliminar boton

--- app_Go/main.go
+++ app_Go/main.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 	"os"
 	"path/filepath"
+	"time"
 )
 
 type Instancia struct {
@@ -67,7 +68,7 @@ func publicarHandler(w http.ResponseWriter, r *http.Request) {
 	//obtener url
 	url := fmt.Sprintf("http://%s/", ip) // de prueba
 	//obtener fecha
-	fecha := "10-10-2010 17:02:02" //de prueba
+	fecha := time.Now().Format("2006-01-02 15:04:05") //de prueba
 
 	// Solicitar maquina virtual por host y mandar el .zip con SCP Y SSH
 
@@ -89,6 +90,7 @@ func publicarHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func eliminarHandler(w http.ResponseWriter, r *http.Request) {
+
 	//script para eliminar maquina virtual o solo .zip?
 }
 
